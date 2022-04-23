0de775696f9a63de3e94a141a799c81315ce15b6
Johan <jsbollll@gmail.com>
2022-04-23 16:36:13 +0200

    changed tabs to spaces

--- main.cpp
+++ main.cpp
@@ -14,51 +14,50 @@ volatile bool click_toggle = false;
 
 LRESULT __stdcall HookCallback(int nCode, WPARAM wParam, LPARAM lParam)
 {
-	if (nCode >= 0)
-	{
-		// the action is valid: HC_ACTION.
-		if (wParam == WM_KEYDOWN)
-		{
-			// lParam is the pointer to the struct containing the data needed, so cast and assign it to kdbStruct.
-			kbdStruct = *((KBDLLHOOKSTRUCT*)lParam);
-			// a key (non-system) is pressed.
-			//fprintf(stderr, "%lu is pressed\n", kbdStruct.vkCode);
+    if (nCode >= 0)
+    {
+        if (wParam == WM_KEYDOWN)
+        {
+            // lParam is the pointer to the struct containing the data needed, so cast and assign it to kdbStruct.
+            kbdStruct = *((KBDLLHOOKSTRUCT*)lParam);
+            // a key (non-system) is pressed.
+            //fprintf(stderr, "%lu is pressed\n", kbdStruct.vkCode);
 
-			if(kbdStruct.vkCode == TOGGLE_KEY)
-			{
-				if(click_toggle)
-				{
-					printf("click off\n");
-					click_toggle = false;
-				}
-				else
-				{
-					printf("click on\n");
-					click_toggle = true;
-				}
-			}
-		}
-	}
+            if(kbdStruct.vkCode == TOGGLE_KEY)
+            {
+                if(click_toggle)
+                {
+                    printf("click off\n");
+                    click_toggle = false;
+                }
+                else
+                {
+                    printf("click on\n");
+                    click_toggle = true;
+                }
+            }
+        }
+    }
  
-	// call the next hook in the hook chain. This is nessecary or your hook chain will break and the hook stops
-	return CallNextHookEx(_hook, nCode, wParam, lParam);
+    // call the next hook in the hook chain. This is nessecary or your hook chain will break and the hook stops
+    return CallNextHookEx(_hook, nCode, wParam, lParam);
 }
 
 
 void SetHook()
 {
 
-	if (!(_hook = SetWindowsHookEx(WH_KEYBOARD_LL, HookCallback, NULL, 0)))
-	{
-		fprintf(stderr, "hook failed");
-		exit(1);
-	}
+    if (!(_hook = SetWindowsHookEx(WH_KEYBOARD_LL, HookCallback, NULL, 0)))
+    {
+        fprintf(stderr, "hook failed");
+    exit(1);
+    }
 }
 
 
 void ReleaseHook()
 {
-	UnhookWindowsHookEx(_hook);
+    UnhookWindowsHookEx(_hook);
 }
 
 
@@ -74,12 +73,12 @@ uint64_t get_micro_time()
 
 DWORD WINAPI toggle_thread(LPVOID lpParam)
 {
-	SetHook();
-	MSG msg;
-	while(GetMessage(&msg, NULL, 0, 0))
-	{}
+    SetHook();
+    MSG msg;
+    while(GetMessage(&msg, NULL, 0, 0))
+    {}
 
-	return 0;
+    return 0;
 }
 
 
@@ -89,44 +88,44 @@ int main(int argc, char *argv[])
 {   
     
 
-	HANDLE thread1 = CreateThread(NULL, 0, toggle_thread, 0, 0, NULL);
-	
-	INPUT input;
-	input.type=INPUT_MOUSE;
-	input.mi.dx=0; 
-	input.mi.dy=0;
-	input.mi.mouseData=0;
-	input.mi.dwExtraInfo=NULL;
-	input.mi.time=0;
+    HANDLE thread1 = CreateThread(NULL, 0, toggle_thread, 0, 0, NULL);
+    
+    INPUT input;
+    input.type=INPUT_MOUSE;
+    input.mi.dx=0; 
+    input.mi.dy=0;
+    input.mi.mouseData=0;
+    input.mi.dwExtraInfo=NULL;
+    input.mi.time=0;
 
-	uint64_t target_time = 1000000 / CLICKS_PER_SECOND;
+    uint64_t target_time = 1000000 / CLICKS_PER_SECOND;
 
-	uint64_t curr;
+    uint64_t curr;
     uint64_t prev = get_micro_time();
     uint64_t dt; // dt in microseconds 10^-6 seconds
 
-	while (1)
-	{
-		curr = get_micro_time();
+    while (1)
+    {
+        curr = get_micro_time();
         dt = curr - prev;
         prev = curr;
 
-		if(click_toggle)
-		{
-			input.mi.dwFlags=(MOUSEEVENTF_LEFTDOWN);
-			SendInput(1, &input, sizeof(INPUT));
+        if(click_toggle)
+        {
+            input.mi.dwFlags=(MOUSEEVENTF_LEFTDOWN);
+            SendInput(1, &input, sizeof(INPUT));
 
-			input.mi.dwFlags=(MOUSEEVENTF_LEFTUP);
-			SendInput(1, &input, sizeof(INPUT));
-		}
+            input.mi.dwFlags=(MOUSEEVENTF_LEFTUP);
+            SendInput(1, &input, sizeof(INPUT));
+        }
 
 
-		do
+        do
         {
             curr = get_micro_time();
             dt = curr - prev;
         } while(dt < target_time);
-	}
+    }
 
-	return 0;
+    return 0;
 }
